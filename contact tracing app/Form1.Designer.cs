eb5a9e112597da00193d81f26e8c09bf59f1b91e
Maria Angela Libre <mariaangelaplibre@iskolarngbayan.pup.edu.ph>
2021-06-03 04:25:35 +0800

    form design

--- contact tracing app/Form1.Designer.cs
+++ contact tracing app/Form1.Designer.cs
@@ -37,16 +37,22 @@ namespace contact_tracing_app
             this.phonelbl = new System.Windows.Forms.Label();
             this.agelbl = new System.Windows.Forms.Label();
             this.agetxtbx = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.phonetxtbx = new System.Windows.Forms.TextBox();
+            this.genderlbl = new System.Windows.Forms.Label();
+            this.contact = new System.Windows.Forms.Label();
+            this.malechk = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.femalechk = new System.Windows.Forms.CheckBox();
+            this.gendertxtbx = new System.Windows.Forms.TextBox();
+            this.addresstxtbx = new System.Windows.Forms.TextBox();
+            this.EMAILLBL = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // firsttxtbx
             // 
-            this.firsttxtbx.Location = new System.Drawing.Point(90, 16);
+            this.firsttxtbx.Location = new System.Drawing.Point(113, 43);
             this.firsttxtbx.Name = "firsttxtbx";
             this.firsttxtbx.Size = new System.Drawing.Size(280, 20);
             this.firsttxtbx.TabIndex = 0;
@@ -54,7 +60,7 @@ namespace contact_tracing_app
             // firstlbl
             // 
             this.firstlbl.AutoSize = true;
-            this.firstlbl.Location = new System.Drawing.Point(12, 19);
+            this.firstlbl.Location = new System.Drawing.Point(12, 46);
             this.firstlbl.Name = "firstlbl";
             this.firstlbl.Size = new System.Drawing.Size(72, 13);
             this.firstlbl.TabIndex = 1;
@@ -62,7 +68,7 @@ namespace contact_tracing_app
             // 
             // lasttextbx
             // 
-            this.lasttextbx.Location = new System.Drawing.Point(90, 61);
+            this.lasttextbx.Location = new System.Drawing.Point(113, 83);
             this.lasttextbx.Name = "lasttextbx";
             this.lasttextbx.Size = new System.Drawing.Size(280, 20);
             this.lasttextbx.TabIndex = 2;
@@ -70,7 +76,7 @@ namespace contact_tracing_app
             // lastlbl
             // 
             this.lastlbl.AutoSize = true;
-            this.lastlbl.Location = new System.Drawing.Point(12, 68);
+            this.lastlbl.Location = new System.Drawing.Point(12, 85);
             this.lastlbl.Name = "lastlbl";
             this.lastlbl.Size = new System.Drawing.Size(68, 13);
             this.lastlbl.TabIndex = 3;
@@ -79,7 +85,7 @@ namespace contact_tracing_app
             // addresslbl
             // 
             this.addresslbl.AutoSize = true;
-            this.addresslbl.Location = new System.Drawing.Point(12, 157);
+            this.addresslbl.Location = new System.Drawing.Point(12, 263);
             this.addresslbl.Name = "addresslbl";
             this.addresslbl.Size = new System.Drawing.Size(59, 13);
             this.addresslbl.TabIndex = 4;
@@ -88,7 +94,7 @@ namespace contact_tracing_app
             // phonelbl
             // 
             this.phonelbl.AutoSize = true;
-            this.phonelbl.Location = new System.Drawing.Point(12, 199);
+            this.phonelbl.Location = new System.Drawing.Point(12, 295);
             this.phonelbl.Name = "phonelbl";
             this.phonelbl.Size = new System.Drawing.Size(95, 13);
             this.phonelbl.TabIndex = 5;
@@ -97,7 +103,7 @@ namespace contact_tracing_app
             // agelbl
             // 
             this.agelbl.AutoSize = true;
-            this.agelbl.Location = new System.Drawing.Point(12, 111);
+            this.agelbl.Location = new System.Drawing.Point(12, 127);
             this.agelbl.Name = "agelbl";
             this.agelbl.Size = new System.Drawing.Size(29, 13);
             this.agelbl.TabIndex = 6;
@@ -105,60 +111,119 @@ namespace contact_tracing_app
             // 
             // agetxtbx
             // 
-            this.agetxtbx.Location = new System.Drawing.Point(90, 104);
+            this.agetxtbx.Location = new System.Drawing.Point(113, 123);
             this.agetxtbx.Name = "agetxtbx";
             this.agetxtbx.Size = new System.Drawing.Size(280, 20);
             this.agetxtbx.TabIndex = 7;
             // 
-            // textBox2
+            // phonetxtbx
             // 
-            this.textBox2.Location = new System.Drawing.Point(113, 240);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(280, 20);
-            this.textBox2.TabIndex = 8;
+            this.phonetxtbx.Location = new System.Drawing.Point(113, 292);
+            this.phonetxtbx.Name = "phonetxtbx";
+            this.phonetxtbx.Size = new System.Drawing.Size(280, 20);
+            this.phonetxtbx.TabIndex = 8;
+            // 
+            // genderlbl
+            // 
+            this.genderlbl.AutoSize = true;
+            this.genderlbl.Location = new System.Drawing.Point(12, 170);
+            this.genderlbl.Name = "genderlbl";
+            this.genderlbl.Size = new System.Drawing.Size(53, 13);
+            this.genderlbl.TabIndex = 10;
+            this.genderlbl.Text = "GENDER";
+            // 
+            // contact
+            // 
+            this.contact.AutoSize = true;
+            this.contact.Location = new System.Drawing.Point(12, 9);
+            this.contact.Name = "contact";
+            this.contact.Size = new System.Drawing.Size(109, 13);
+            this.contact.TabIndex = 11;
+            this.contact.Text = "CONTACT TRACING";
+            // 
+            // malechk
+            // 
+            this.malechk.AutoSize = true;
+            this.malechk.Location = new System.Drawing.Point(113, 170);
+            this.malechk.Name = "malechk";
+            this.malechk.Size = new System.Drawing.Size(55, 17);
+            this.malechk.TabIndex = 12;
+            this.malechk.Text = "MALE";
+            this.malechk.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(113, 197);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(140, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "OTHER (please specify)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // femalechk
+            // 
+            this.femalechk.AutoSize = true;
+            this.femalechk.Location = new System.Drawing.Point(192, 170);
+            this.femalechk.Name = "femalechk";
+            this.femalechk.Size = new System.Drawing.Size(68, 17);
+            this.femalechk.TabIndex = 14;
+            this.femalechk.Text = "FEMALE";
+            this.femalechk.UseVisualStyleBackColor = true;
+            // 
+            // gendertxtbx
+            // 
+            this.gendertxtbx.Location = new System.Drawing.Point(113, 220);
+            this.gendertxtbx.Name = "gendertxtbx";
+            this.gendertxtbx.Size = new System.Drawing.Size(280, 20);
+            this.gendertxtbx.TabIndex = 8;
+            // 
+            // addresstxtbx
+            // 
+            this.addresstxtbx.Location = new System.Drawing.Point(113, 256);
+            this.addresstxtbx.Name = "addresstxtbx";
+            this.addresstxtbx.Size = new System.Drawing.Size(280, 20);
+            this.addresstxtbx.TabIndex = 9;
+            // 
+            // EMAILLBL
+            // 
+            this.EMAILLBL.AutoSize = true;
+            this.EMAILLBL.Location = new System.Drawing.Point(12, 331);
+            this.EMAILLBL.Name = "EMAILLBL";
+            this.EMAILLBL.Size = new System.Drawing.Size(94, 13);
+            this.EMAILLBL.TabIndex = 15;
+            this.EMAILLBL.Text = "EMAIL ADDRESS";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
+            this.textBox1.TabIndex = 16;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(113, 196);
+            this.textBox3.Location = new System.Drawing.Point(112, 328);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(280, 20);
-            this.textBox3.TabIndex = 8;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(90, 150);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(280, 20);
-            this.textBox4.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 288);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 288);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
+            this.textBox3.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 310);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
+            this.ClientSize = new System.Drawing.Size(412, 367);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EMAILLBL);
+            this.Controls.Add(this.femalechk);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.malechk);
+            this.Controls.Add(this.contact);
+            this.Controls.Add(this.genderlbl);
+            this.Controls.Add(this.addresstxtbx);
+            this.Controls.Add(this.phonetxtbx);
+            this.Controls.Add(this.gendertxtbx);
             this.Controls.Add(this.agetxtbx);
             this.Controls.Add(this.agelbl);
             this.Controls.Add(this.phonelbl);
@@ -184,11 +249,17 @@ namespace contact_tracing_app
         private System.Windows.Forms.Label phonelbl;
         private System.Windows.Forms.Label agelbl;
         private System.Windows.Forms.TextBox agetxtbx;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox phonetxtbx;
+        private System.Windows.Forms.Label genderlbl;
+        private System.Windows.Forms.Label contact;
+        private System.Windows.Forms.CheckBox malechk;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox femalechk;
+        private System.Windows.Forms.TextBox gendertxtbx;
+        private System.Windows.Forms.TextBox addresstxtbx;
+        private System.Windows.Forms.Label EMAILLBL;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
